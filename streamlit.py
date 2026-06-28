942475716206e036a2d1f8a3bc4b4bf9b424c59f
Aishath Risla Umar <aishath.risla@stats.gov.mv>
2026-06-28 10:29:43 +0500

    updated

--- streamlit.py
+++ streamlit.py
@@ -15,25 +15,35 @@ project_1_page = st.Page(
     default=True
 )
 
-project_2_page = st.Page(
-    page="views/timeuse_summary.py",
-    title="Time Use Summary",
-    icon=":material/overview:"
-)
+# project_2_page = st.Page(
+#     page="views/timeuse_summary.py",
+#     title="Time Use Summary",
+#     icon=":material/overview:"
+# )
 
-project_3_page = st.Page(
-    page="views/status_summary.py",
-    title="Status Summary",
-    icon=":material/overview:"
-)
+# project_3_page = st.Page(
+#     page="views/status_summary.py",
+#     title="Status Summary",
+#     icon=":material/overview:"
+# )
+
+# # NAVIGATION SETUP 
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page, project_3_page]) # (WITHOUT SECTION)
+
+# pg = st.navigation(
+#     {
+#         "Info": [about_page],
+#         "Projects": [project_1_page, project_2_page, project_3_page]
+#     }
+# ) # (WITH SECTION)
 
 # NAVIGATION SETUP 
-pg = st.navigation(pages=[about_page, project_1_page, project_2_page, project_3_page]) # (WITHOUT SECTION)
+pg = st.navigation(pages=[about_page, project_1_page]) # (WITHOUT SECTION)
 
 pg = st.navigation(
     {
         "Info": [about_page],
-        "Projects": [project_1_page, project_2_page, project_3_page]
+        "Projects": [project_1_page]
     }
 ) # (WITH SECTION)
 
