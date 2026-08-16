5d33ed612405bf81ae4e0f73d01d045971ea8d1a
Aishath Risla Umar <aishath.risla@stats.gov.mv>
2026-08-16 08:50:20 +0500

    updated

--- views/survey_progress.py
+++ views/survey_progress.py
@@ -24,7 +24,7 @@ st.title("HIES and TUS Progress")
 print(datetime.now())
 
 
-fixed_time = datetime(2026,8,11 ,14, 30,00)
+fixed_time = datetime(2026,8,16 ,8, 35,00)
 
 st.markdown(
     f"***Last updated on: 📅 {fixed_time.strftime('%A, %d %B %Y %H:%M:%S')}***"
